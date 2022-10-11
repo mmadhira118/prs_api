d27c9b3268a12376d2bcece431853507fa3a63d4
mmadhira118 <maheshr.sidgs@gmail.com>
2022-10-11 12:18:32 +0530

    Code Formated

--- functions/src/propertyController.ts
+++ functions/src/propertyController.ts
@@ -17,7 +17,7 @@ const getAllProperties = async (req: Request, res: Response) => {
     const querySnapshot = await db.collection('property_details').get()
     querySnapshot.forEach((doc: any) => allProperties.push(doc.data()))
     return res.status(200).json(allProperties)
-  } catch(error) { return res.status(500).send('Something broke!') }
+  } catch (error) { return res.status(500).send('Something broke!') }
 }
 
 export { getAllProperties }
